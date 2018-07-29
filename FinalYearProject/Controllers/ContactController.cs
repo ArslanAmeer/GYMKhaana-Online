7e4e77b39803f14c51072ed90a2cbc3884e04619
ArslanAmeer <arslan.ameer@hotmail.com>
2018-07-29 10:44:48 +0500

    Added New Contact Controller with Diffrent Action Methods

---
+++ FinalYearProject/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using FinalProjectClasses;
+using FinalProjectClasses.GymMngmnt;
+using System.Web.Mvc;
+
+namespace FinalYearProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SubmitMessage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SubmitMessage(Contact contact)
+        {
+            Dbcontext db = new Dbcontext();
+            using (db)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("", "");
+            }
+        }
+
+        public ActionResult SuccessMessage()
+        {
+            return View();
+        }
+    }
+}
