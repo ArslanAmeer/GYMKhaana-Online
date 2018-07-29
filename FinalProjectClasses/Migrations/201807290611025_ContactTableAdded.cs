c41a819a2e7d811c7106004293cadd80fe203127
ArslanAmeer <arslan.ameer@hotmail.com>
2018-07-29 11:14:30 +0500

    Update the Migration Added the New Table of the contact table

---
+++ FinalProjectClasses/Migrations/201807290611025_ContactTableAdded.cs
@@ -0,0 +1,30 @@
+namespace FinalProjectClasses.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ContactTableAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        PhoneNo = c.Long(nullable: false),
+                        Subject = c.String(),
+                        Message = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contacts");
+        }
+    }
+}
