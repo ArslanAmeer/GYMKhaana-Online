e2c327fd83bcb2ee4233023ecebab3c3253093b8
ArslanAmeer <arslan.ameer@hotmail.com>
2018-08-05 00:48:15 +0500

    Update the Atrribute of the instructer class

--- FinalProjectClasses/GymMngmnt/Instructer.cs
+++ FinalProjectClasses/GymMngmnt/Instructer.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FinalProjectClasses.GymMngmnt
+﻿namespace FinalProjectClasses.GymMngmnt
 {
-    class Instructer
+    public class Instructer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Experience { get; set; }
+        public string ImageUrl { get; set; }
+        public string Achivements { get; set; }
+
     }
 }
