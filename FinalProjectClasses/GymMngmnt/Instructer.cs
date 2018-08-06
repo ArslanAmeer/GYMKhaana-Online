cb2eef2b6d8b488fef3a342835c90c3a2b88b77b
ArslanAmeer <arslan.ameer@hotmail.com>
2018-08-06 10:36:36 +0500

    Update the instruter class validatin

--- FinalProjectClasses/GymMngmnt/Instructer.cs
+++ FinalProjectClasses/GymMngmnt/Instructer.cs
@@ -1,14 +1,23 @@
-﻿namespace FinalProjectClasses.GymMngmnt
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProjectClasses.GymMngmnt
 {
     public class Instructer
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Experience { get; set; }
+        [Required]
         public int Salary { get; set; }
+        [Required]
         public string FullAddress { get; set; }
+        [Required]
         public long PhoneNo { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
         public string Achivements { get; set; }
 
 
