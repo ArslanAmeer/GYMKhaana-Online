442047f34ef2872b69fb718a699c66da530840fd
ArslanAmeer <arslan.ameer@hotmail.com>
2018-08-06 10:53:52 +0500

    Update:Added Code Uploading Image

--- FinalYearProject/Controllers/InstructorController.cs
+++ FinalYearProject/Controllers/InstructorController.cs
@@ -1,5 +1,7 @@
 ﻿using FinalProjectClasses;
 using FinalProjectClasses.GymMngmnt;
+using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace FinalYearProject.Controllers
@@ -20,6 +22,22 @@ namespace FinalYearProject.Controllers
         public ActionResult HireInstructor(Instructer instructer)
         {
             Dbcontext db = new Dbcontext();
+            int counter = 0;
+            long uno = DateTime.Now.Ticks;
+
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                if (!string.IsNullOrEmpty(file.FileName))
+                {
+                    string abc = uno + "_" + ++counter +
+                                 file.FileName.Substring(file.FileName.LastIndexOf("."));
+                    string url = "~/Content/InstructorImages/" + abc;
+                    string path = Request.MapPath(url);
+                    instructer.ImageUrl = abc;
+                    file.SaveAs(path);
+                }
+            }
             using (db)
             {
                 db.Instructers.Add(instructer);
