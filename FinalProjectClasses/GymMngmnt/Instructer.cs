eefe0d4a184e4384f9a7488064973613fe876991
ArslanAmeer <arslan.ameer@hotmail.com>
2018-08-06 09:48:24 +0500

    Update the instructer class propertise

--- FinalProjectClasses/GymMngmnt/Instructer.cs
+++ FinalProjectClasses/GymMngmnt/Instructer.cs
@@ -5,8 +5,12 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Experience { get; set; }
+        public int Salary { get; set; }
+        public string FullAddress { get; set; }
+        public long PhoneNo { get; set; }
         public string ImageUrl { get; set; }
         public string Achivements { get; set; }
+
 
     }
 }
